2132d2adec889dd3edbe59dbbd435cb96665a978
Psyrax <psyrax@sclbits.com>
2015-10-01 19:04:51 -0500

    init

---
+++ database/migrations/2015_10_01_233155_create_image_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('path');
+            $table->string('prevId')->nullable();
+            $table->string('title')->nullable();
+            $table->longText('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('images');
+    }
+}
